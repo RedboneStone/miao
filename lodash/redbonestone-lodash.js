85c73c4d1da56d9a3700d0fcbf657a35568076e4
RedboneStone <Redbonestone123@gmail.com>
2019-01-10 23:11:30 +0800

    chunk

--- lodash/redbonestone-lodash.js
+++ lodash/redbonestone-lodash.js
@@ -1,31 +1,44 @@
 
-var redbonestone = {
-	chunk: function chunk(ary, size = 1) {
-		var result = []
-		for(var i = 0; i< ??; i++) {
-			result.push(ary.slice(i, i + size))
+var redbonestone = (function() {
+	function chunk(ary, size = 1) {
+		let result = [];
+		for(let i = 0; i < ary.length;i+=size) {
+			f(i+size > ary.length){
+				result.push(ary.slice(i,ary.length));
+				break;
+			}
+			result.push(ary.slice(i,i+size));
 		}
 		return result
-	},
-	compact: function(ary) {
-		var result = []
-		for(var item of ary) {
-			if (item) {
-				result.push(item)
+	}
+	function uniq(ary) {
+		let tmp = {};
+		for(let i = 0;i<ary.length;i++){
+			if(ary[i] in tmp){
+				tmp[ary[i]]++
+			} else {
+				tmp[ary[i]] = 1
 			}
 		}
-		return result
-	},
-	uniq: function uniq(ary) {
-				let tmp = {};
-			  for(let i = 0;i<ary.length;i++){
-			  	if(ary[i] in tmp){
-			  		  tmp[ary[i]]++
-				  } else {
-			  		  tmp[ary[i]] = 1
-				  }
-			  }
-			  return Object.keys(tmp);
+		return Object.keys(tmp);
 	}
-};
+	function compact() {
+
+	}
+	function difference(){
+
+	}
+	function concat() {
+
+	}
+	return {
+		chunk: chunk,
+		compact: compact,
+		concat:concat,
+		difference:difference,
+		uniq:uniq,
+	};
+})();
+
+
 
