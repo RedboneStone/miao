ffa3efec1dae0105ca36a376d2cd1eae5bbc310a
RedboneStone <Redbonestone123@gmail.com>
2019-03-08 00:32:27 +0800

    concat()

--- lodash/redbonestone-lodash.js
+++ lodash/redbonestone-lodash.js
@@ -10,40 +10,77 @@ var redbonestone = (function() {
 			result.push(ary.slice(i,i+size));
 		}
 		return result
-	}
+	}//chunk 分块  将数组按照规定的个数分块
 	function uniq(ary) {
 		return ary.filter((element,index,ary)=> ary.indexOf(element) === index);
 	}
 	function uniqby(){
 
 	}
+
+
+	/*  _.compact 
+		:Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+	*/
 	function compact(arr) {
+		// var result = [];
+		// for(let i = 0;i < arr.length;i++){
+		// 	if(arr[i]){
+		// 		result.push(arr[i]);
+		// 	}
+		// }
+		// return result;
+		var index = -1;
+		var length = array == null ? 0 : array.length;
+		var resIndex = 0;
 		var result = [];
-		for(let i = 0;i < arr.length;i++){
-			if(arr[i]){
-				result.push(arr[i]);
+
+		while (++index < length) {
+			var value = array[index];
+			if (value) {
+				result[resIndex++] = value;
+			  }
+			  // ++index
+		}
+			return result;
+	}// compact 简洁的 紧密的 结实的
+
+
+	function difference(ary,...values){
+		let result = [];
+		let tmp = flatten(values);
+		for(let i = 0;i < ary.length;i++) {
+			for (let j = 0; j < tmp.length; j++) {
+				if(ary[i] !== tmp[j]){
+					if(j == tmp.length - 1){
+						 result.push(ary[i]);
+					}
+					continue;
+				}
+				 break;
 			}
 		}
 		return result;
 	}
-	function difference(ary,...values){
-		let result = [];
-		for(let i = 0;i<ary.length;i++) {
-			for (let j = 0; j < values.length; j++) {
-				if (ary[i] !== arguments[1][j]) {
-					result.push(i);
-					break;
-				}
-			}
-		}
-		return result.map(it => ary[it]);
-	}
 	function differenceby(){
 
 	}
-	function concat() {
+	/**
+	 * _.concat  拼接数组
+	 *    Creates a new array concatenating array with any additional arrays and/or values.
+	 */
+	function concat(ary,...values) {
+		var result; 
+		for(let i of values){
+			if(!Array.isArray(i)){
+				result = ary.push(i)
+			} else {
+				result = ary.push(i.flatten())
+			}
 
-	}
+		 }
+		 return result 
+	}// 需要判断数组为二元数组时的情况
 	function keys(){
 
 	}
